099cc13970d749c95997d3a520d9a62cb2892856
yasuko06 <153053254+yasuko06@users.noreply.github.com>
2024-06-04 19:10:20 +0900

    Add files via upload
    課題７

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<?php
+// 配列
+$food = ['name'=> 'onion', 'price' => 200, 'weight' => 160];
+
+
+// 連想配列の値を出力する
+print_r($food);
+
+?>
+
+</body>
+</html>
